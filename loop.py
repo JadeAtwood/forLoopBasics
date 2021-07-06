1910aca0321dbbfbe17974a72a0b127709c77f1e
Jade Atwood <jadeatwood@protonmail.com>
2021-07-06 14:40:04 -0400

    first try

---
+++ loop.py
@@ -0,0 +1,37 @@
+# basics
+# for x in range (0, 151, 1):
+#     print(x)
+
+#  x5's   to 1k
+# for i in range(5, 1005, 5):
+#     print(i)
+
+#  counting the dojo way 10 = coding dojo  5= dojo
+# for y in range(1,  101, 1):
+#     if  y %10  ==  0:
+#         print("Coding Dojo")
+#     elif y%5 ==0:
+#         print("Codong")
+#     else:
+#         print(y)
+
+# num43 = 0
+#  Whoa that sucker is huge add odds 0-500k  
+#  counts evry iteration
+# for p in range(1, 500001, 2):
+
+#     num43 = num43  + p
+#     print(p, num43)
+
+#  Countdown by 4
+# for m in range(2018, 0, -4):
+#     print(m)
+
+#  Flexible Counter
+# lowNum = 4
+# highNum =36
+# mult = 3
+
+# for q in range(lowNum ,highNum+1, 1):
+#     if q %mult == 0:
+#         print(q)
